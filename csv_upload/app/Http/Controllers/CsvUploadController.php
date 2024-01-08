8825a06daa7b0ef85eafd2decc3aeca91be47327
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2024-01-08 21:51:23 +0630

    duplicate csv

---
+++ csv_upload/app/Http/Controllers/CsvUploadController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PDO;
+
+class CsvUploadController extends Controller
+{
+    public function index()
+    {
+        return view('upload');
+    }
+
+    public function progress()
+    {
+        return view('progress');
+    }
+
+    public function uploadFileAndStoreDatabase(Request $request)
+    {
+        try {
+            if ($request->hasFile('csvFile')) {
+                $fileName = $request->csvFile->getClientOriginalName();
+                 $filWithPath=public_path('uploads').'/'.$fileName;
+                 if(!file_exists($filWithPath)){
+                    $request->file('csvFile')->move(public_path('uploads'), $fileName);
+                 }
+                 $header=null;
+                 $dataFromcsv=array();
+                 $records=array_map('str_getcsv',file($filWithPath));
+                 
+
+                 foreach($records as $record){
+                    if(!$header){
+                        $header =$record;
+                    }else {
+                        $dataFromcsv[]=$record;
+                    }
+                 }
+                 $dataFromcsv=array_chunk($dataFromcsv,300);
+                 foreach($dataFromcsv as $index=>$dataCsv){
+                    foreach($dataCsv as $data){
+                        $employeeData[$index][]= array_combine($header,$data);
+                    }
+                 }
+                 dd($employeeData);
+               
+            }
+            //   dd($request->all());
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
--- csv_upload/routes/web.php
+++ csv_upload/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CsvUploadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/upload',[CsvUploadController::class,'index'])->name('upload');
+Route::get('/progress',[CsvUploadController::class,'progress'])->name('progress');
+Route::post('/upload',[CsvUploadController::class,'uploadFileAndStoreDatabase'])->name('processFile');
